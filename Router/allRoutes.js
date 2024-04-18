e48a7f6c1faff488689b1be7aad0409c4f61f1f1
gerybeltrame <gery.beltrame@gmail.com>
2024-04-18 18:29:28 +0200

    nouvelles routes ajoutées

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,8 +1,15 @@
 import Route from "./Route.js";
 
 //Définir ici vos routes
-export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+export const allRoutes =
+    [new Route("/", "Accueil", "/pages/home.html"),];
+    [new Route("/", "Services", "/pages/home.html"),];
+    [new Route("/", "Habitats", "/pages/home.html"),];
+    [new Route("/", "Contacts", "/pages/home.html"),];
+    [new Route("/", "Connexion", "/pages/home.html"),];
+    [new Route("/", "Page administration", "/pages/home.html"),];
+    [new Route("/", "Page vétérinaire", "/pages/home.html"),];
+    [new Route("/", "Page employé", "/pages/home.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
-export const websiteName = "Quai Antique";
+export const websiteName = "Zoo Arcadia";
