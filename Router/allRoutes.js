e127690fd70b5ab7ef880585b7e10ae03ad7a56b
gerybeltrame <gery.beltrame@gmail.com>
2024-04-18 19:30:18 +0200

    modification du routage

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,14 +2,14 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes =
-    [new Route("/", "Accueil", "/pages/home.html"),];
-    [new Route("/", "Services", "/pages/home.html"),];
-    [new Route("/", "Habitats", "/pages/home.html"),];
-    [new Route("/", "Contacts", "/pages/home.html"),];
-    [new Route("/", "Connexion", "/pages/home.html"),];
-    [new Route("/", "Page administration", "/pages/home.html"),];
-    [new Route("/", "Page vétérinaire", "/pages/home.html"),];
-    [new Route("/", "Page employé", "/pages/home.html"),];
+    [new Route("/", "Accueil", "/pages/Accueil.html"),];
+    [new Route("/Services", "Services", "/pages/Services.html"),];
+    [new Route("/Services", "Habitats", "/pages/Habitats.html"),];
+    [new Route("/Contacts", "Contacts", "/pages/Contacts.html"),];
+    [new Route("/Connexion", "Connexion", "/pages/Connexion.html"),];
+    [new Route("/Page_admin", "Page administration", "/pages/Page_admin.html"),];
+    [new Route("/Page_vétérinaire", "Page vétérinaire", "/pages/Page_vétérinaire.html"),];
+    [new Route("/Page_employé", "Page employé", "/pages/Page_employé.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Zoo Arcadia";
