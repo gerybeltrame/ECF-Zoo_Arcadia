1a0abc4ade7777bcb3154efd1da3ec74f05289a3
gerybeltrame <gery.beltrame@gmail.com>
2024-04-24 17:13:47 +0200

    Ajout bannière à toutes les pages + problèmes de routages régler

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,14 +2,14 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/Services", "Services", "/pages/Services.html"),
-    new Route("/Habitats", "Habitats", "/pages/Habitats.html"),
-    new Route("/Contacts", "Contacts", "/pages/Contacts.html"),
-    new Route("/Connexion", "Connexion", "/pages/Connexion.html"),
-    new Route("/Page_admin", "Page administration", "/pages/Page_admin.html"),
-    new Route("/Page_vétérinaire", "Page vétérinaire", "/pages/Page_vétérinaire.html"),
-    new Route("/Page_employé", "Page employé", "/pages/Page_employé.html")
+    new Route("/", "Accueil", "/Pages/home.html"),
+    new Route("/Services", "Services", "/Pages/Services.html"),
+    new Route("/Habitats", "Habitats", "/Pages/Habitats.html"),
+    new Route("/Contacts", "Contacts", "/Pages/Contacts.html"),
+    new Route("/Connexion", "Connexion", "/Pages/Connexion.html"),
+    new Route("/Page_admin", "Page administration", "/Pages/Page_admin.html"),
+    new Route("/Page_vétérinaire", "Page vétérinaire", "/Pages/Page_vétérinaire.html"),
+    new Route("/Page_employé", "Page employé", "/Pages/Page_employé.html")
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
