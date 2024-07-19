abbfb951e442c70f733152bb8faf18595f6d2561
gerybeltrame <gery.beltrame@gmail.com>
2024-07-19 17:34:06 +0200

    correction

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,10 +7,10 @@ export const allRoutes = [
     new Route("/Habitats", "Habitats", "/Pages/Habitats.html", []),
     new Route("/Contacts", "Contacts", "/Pages/Contacts.html", []),
     new Route("/Connexion", "Connexion", "/Pages/Connexion.html", ["disconnected"],"/js/auth/signin.js"),
-    new Route("/Page_admin", "Page administration", "/Pages/Page_admin.html", ["admin"], "/js/auth/signup.js"),
-    new Route("/Animaux_savane", "Animaux savane", "/Pages/Animaux_savane.html", []),
-    new Route("/Animaux_jungle", "Animaux jungle", "/Pages/Animaux_jungle.html", []),
-    new Route("/Animaux_marais", "Animaux marais", "/Pages/Animaux_marais.html", []),
+    new Route("/Page_admin", "Pageadministration", "/Pages/Page_admin.html", ["admin"], "/js/auth/signup.js"),
+    new Route("/Animaux_savane", "Animauxsavane", "/Pages/Animaux_savane.html", []),
+    new Route("/Animaux_jungle", "Animauxjungle", "/Pages/Animaux_jungle.html", []),
+    new Route("/Animaux_marais", "Animauxmarais", "/Pages/Animaux_marais.html", []),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
